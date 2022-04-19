b2685137a523b699da8c60330360a8961539b2cd
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-04-19 19:18:55 +0300

    оно рисуется

--- main.py
+++ main.py
@@ -44,24 +44,30 @@ class TicTacToe(Board):
 
     def on_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
-        if self.current_turn == 1:
-            self.current_turn = 2
-        else:
-            self.current_turn = 1
         x = cell[0]
         y = cell[1]
         print(x, y)
         if self.board[y][x] == 0:
+            if self.current_turn == 1:
+                self.current_turn = 2
+            else:
+                self.current_turn = 1
             self.board[y][x] = self.current_turn
         else:
-            print('Эта клетка уже занята')  # переделать так, чтобы ход не менялся при нажатии на не пустую клетку
+            print('Эта клетка уже занята')
 
     def render(self):
         for y in range(self.height):
             for x in range(self.width):
                 if self.board[y][x] == 1:
-                    pass
-                    # draw X
+                    pygame.draw.line(screen, pygame.Color('red'),
+                                     (x * self.cell_size + self.left, y * self.cell_size + self.top),
+                                     (x * self.cell_size + self.left + self.cell_size,
+                                      y * self.cell_size + self.top + self.cell_size))
+                    pygame.draw.line(screen, pygame.Color('red'),
+                                     (x * self.cell_size + self.left,
+                                      y * self.cell_size + self.top + self.cell_size),
+                                     (x * self.cell_size + self.left + self.cell_size, y * self.cell_size + self.top))
                 if self.board[y][x] == 2:
                     pygame.draw.ellipse(screen, pygame.Color('blue'),
                                         (x * self.cell_size + self.left + 5, y * self.cell_size + self.top + 5,
@@ -74,7 +80,6 @@ class TicTacToe(Board):
 board = TicTacToe(3, 3)
 board.set_view(15, 15, 190)
 running = True
-ticks = 50
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,9 +88,5 @@ while running:
             board.on_click(event.pos)
     screen.fill((0, 0, 0))
     board.render()
-    if ticks == 50:
-        ticks = 0
     pygame.display.flip()
-    clock.tick(50)
-    ticks += 1
 pygame.quit()
