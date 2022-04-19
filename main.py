b391c6e31153ce3d8c29477a9b4234d18c62ff25
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-04-19 15:53:01 +0300

    Начало положено(я точно не использовал старый код)

---
+++ main.py
@@ -0,0 +1,58 @@
+import pygame
+
+pygame.init()
+pygame.display.set_caption('tic_tac_toe_4_zoomers')
+size = [600, 600]
+screen = pygame.display.set_mode(size)
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color('white'), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top,
+                    self.cell_size, self.cell_size), 1)
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def on_click(self, cell):
+        print(cell)
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y > self.height:
+            return None
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+
+board = Board(3, 3)
+board.set_view(15, 15, 190)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
+pygame.quit()
