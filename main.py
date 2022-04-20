4b10e6d1c424b0d9c9c997fb0dc1ec8136a58688
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-04-21 01:47:51 +0300

    Всё. Теперь точно конец

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Board:
         self.top = 10
         self.cell_size = 30
 
+
     def render(self):
         for y in range(self.height):
             for x in range(self.width):
@@ -46,13 +47,11 @@ class TicTacToe(Board):
         cell = self.get_cell(mouse_pos)
         x = cell[0]
         y = cell[1]
-        print(x, y)
         if self.board[y][x] == 0:
-            if self.current_turn == 1:
-                self.current_turn = 2
-            else:
-                self.current_turn = 1
+            print(x, y)
+            self.current_turn *= -1
             self.board[y][x] = self.current_turn
+            self.end()
         else:
             print('Эта клетка уже занята')
 
@@ -68,14 +67,37 @@ class TicTacToe(Board):
                                      (x * self.cell_size + self.left,
                                       y * self.cell_size + self.top + self.cell_size),
                                      (x * self.cell_size + self.left + self.cell_size, y * self.cell_size + self.top))
-                if self.board[y][x] == 2:
+                if self.board[y][x] == -1:
                     pygame.draw.ellipse(screen, pygame.Color('blue'),
                                         (x * self.cell_size + self.left + 5, y * self.cell_size + self.top + 5,
                                          self.cell_size - 10, self.cell_size - 10), 1)
                 pygame.draw.rect(screen, pygame.Color(255, 255, 255),
                                  (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
                                   self.cell_size), 1)
-
+    def end(self):
+        global running
+        clear = False
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if self.board[i][j] == 0:
+                    clear = True
+                if (j == 2):
+                    if self.board[i][j] == self.board[i][j - 1] == self.board[i][j - 2] != 0:
+                        if (self.board[i][j] == 1):
+                            print("Победил игрок 1")
+                        if (self.board[i][j] == -1):
+                            print("Победил игрок 2")
+                        running = False
+                if (i == 2):
+                    if self.board[i][j] == self.board[i - 1][j] == self.board[i - 2][j] != 0:
+                        if (self.board[i][j] == 1):
+                            print("Победил игрок 1")
+                        if (self.board[i][j] == -1):
+                            print("Победил игрок 2")
+                        running = False
+        if (not clear):
+            print("Ничья")
+            running = False
 
 board = TicTacToe(3, 3)
 board.set_view(15, 15, 190)
