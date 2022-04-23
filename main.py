d8bf369a38ec4c527d23517a534a921f52b7d919
WhoReadThisWillDie <78028560+WhoReadThisWillDie@users.noreply.github.com>
2022-04-23 13:47:12 +0300

    Ну прям точно конец

--- main.py
+++ main.py
@@ -11,11 +11,10 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = [[0] * width for _ in range(height)]
+        self.board = [[''] * width for _ in range(height)]
         self.left = 10
         self.top = 10
         self.cell_size = 30
-
 
     def render(self):
         for y in range(self.height):
@@ -34,7 +33,7 @@ class TicTacToe(Board):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.selected_cell = None
-        self.current_turn = 1
+        self.current_turn = 'x'
 
     def get_cell(self, mouse_pos):
         cell_x = (mouse_pos[0] - self.left) // self.cell_size
@@ -47,18 +46,21 @@ class TicTacToe(Board):
         cell = self.get_cell(mouse_pos)
         x = cell[0]
         y = cell[1]
-        if self.board[y][x] == 0:
-            print(x, y)
-            self.current_turn *= -1
+        if self.board[y][x] == '':
             self.board[y][x] = self.current_turn
-            self.end()
+            if self.current_turn == 'x':
+                self.check_winner('x')
+                self.current_turn = 'o'
+            elif self.current_turn == 'o':
+                self.check_winner('o')
+                self.current_turn = 'x'
         else:
             print('Эта клетка уже занята')
 
     def render(self):
         for y in range(self.height):
             for x in range(self.width):
-                if self.board[y][x] == 1:
+                if self.board[y][x] == 'x':
                     pygame.draw.line(screen, pygame.Color('red'),
                                      (x * self.cell_size + self.left, y * self.cell_size + self.top),
                                      (x * self.cell_size + self.left + self.cell_size,
@@ -67,37 +69,39 @@ class TicTacToe(Board):
                                      (x * self.cell_size + self.left,
                                       y * self.cell_size + self.top + self.cell_size),
                                      (x * self.cell_size + self.left + self.cell_size, y * self.cell_size + self.top))
-                if self.board[y][x] == -1:
+                if self.board[y][x] == 'o':
                     pygame.draw.ellipse(screen, pygame.Color('blue'),
                                         (x * self.cell_size + self.left + 5, y * self.cell_size + self.top + 5,
                                          self.cell_size - 10, self.cell_size - 10), 1)
                 pygame.draw.rect(screen, pygame.Color(255, 255, 255),
                                  (x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
                                   self.cell_size), 1)
-    def end(self):
+
+    def check_winner(self, sign):
         global running
-        clear = False
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                if self.board[i][j] == 0:
-                    clear = True
-                if (j == 2):
-                    if self.board[i][j] == self.board[i][j - 1] == self.board[i][j - 2] != 0:
-                        if (self.board[i][j] == 1):
-                            print("Победил игрок 1")
-                        if (self.board[i][j] == -1):
-                            print("Победил игрок 2")
-                        running = False
-                if (i == 2):
-                    if self.board[i][j] == self.board[i - 1][j] == self.board[i - 2][j] != 0:
-                        if (self.board[i][j] == 1):
-                            print("Победил игрок 1")
-                        if (self.board[i][j] == -1):
-                            print("Победил игрок 2")
-                        running = False
-        if (not clear):
-            print("Ничья")
+        win = False
+        empty = 0
+        for row in self.board:
+            empty += row.count('')
+            if row.count(sign) == 3:
+                win = True
+        for col in range(len(self.board[0])):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] == sign:
+                win = True
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == sign:
+            win = True
+        if self.board[2][0] == self.board[1][1] == self.board[0][2] == sign:
+            win = True
+        if win:
+            print(f'Победил игрок {sign}')
             running = False
+        if not win:
+            if empty:
+                pass
+            else:
+                print('Ничья')
+                running = False
+
 
 board = TicTacToe(3, 3)
 board.set_view(15, 15, 190)
